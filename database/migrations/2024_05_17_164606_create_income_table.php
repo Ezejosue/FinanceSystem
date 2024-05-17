3d70c2df41d701c5a2b3b54bfcca8052caeb218a
Josué Ávalos <aezequiel56@gmail.com>
2024-05-17 10:47:20 -0600

    update

---
+++ database/migrations/2024_05_17_164606_create_income_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('income', function (Blueprint $table) {
+            $table->integer('id', true);
+            $table->string('type', 50);
+            $table->decimal('amount', 10);
+            $table->date('date');
+            $table->string('invoice');
+            $table->integer('user_id')->nullable()->index('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('income');
+    }
+};

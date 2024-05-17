035070a98626765a38b7ef0c33bad24b9e19b4f9
Josué Ávalos <aezequiel56@gmail.com>
2024-05-16 20:23:11 -0600

    Documentation with swagger

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,7 +8,30 @@ use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Support\Facades\Hash;
 
 
-
+/**
+ * @OA\Schema(
+ *     schema="User",
+ *     type="object",
+ *     title="User",
+ *     description="User model",
+ *     required={"username", "email", "password"},
+ *     @OA\Property(
+ *         property="username",
+ *         type="string",
+ *         description="Its the name of the user"
+ *     ),
+ *     @OA\Property(
+ *         property="email",
+ *         type="string",
+ *         description="Email of the user"
+ *     ),
+ *     @OA\Property(
+ *         property="password",
+ *         type="string",
+ *         description="Password of the user"
+ *     ),
+ * )
+ */
 class User extends Authenticatable
 {
     use Notifiable, HasApiTokens;

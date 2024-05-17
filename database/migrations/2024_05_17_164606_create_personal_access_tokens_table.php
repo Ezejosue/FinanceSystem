908968f50b5ca645265079e14c34acd1a0dfa7f4
Josué Ávalos <aezequiel56@gmail.com>
2024-05-17 11:08:26 -0600

    Update 2024_05_17_164606_create_personal_access_tokens_table.php

--- database/migrations/2024_05_17_164606_create_personal_access_tokens_table.php
+++ database/migrations/2024_05_17_164606_create_personal_access_tokens_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
+        Schema::dropIfExists('personal_access_tokens');
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('tokenable_type');
